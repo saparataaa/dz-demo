a26b5a5d9ee83ae6d17d79fa9a0ad98a6cbf0d82
saparataaa <eugen2012@internet.ru>
2026-07-06 21:49:32 +0400

    скидка

---
+++ 2-operations/discount.py
@@ -0,0 +1,5 @@
+price = int(input("Введите цену товара:"))
+discount_percent = int(input("Введите процент скидки:"))
+discount_amount = price * (discount_percent / 100)
+final_price = price - discount_amount
+print (f"цена со скидкой"{final_price})
