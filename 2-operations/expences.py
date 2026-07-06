5cc12e61070e4829baa79b5d1a60ec953440a3fc
saparataaa <eugen2012@internet.ru>
2026-07-06 15:35:11 +0400

    сделал

--- 2-operations/expences.py
+++ 2-operations/expences.py
@@ -1,4 +1,7 @@
-eat = input("еда")
-trafic = input("транспорт")
-entertainments = input("развлечения")
-print(eat, trafic, entertainments)
+eat = float(input("еда:"))
+trafic = float(input("транспорт:"))
+entertainments = float(input("развлечения:"))
+total = eat + trafic + entertainments
+average = total / 3
+print(f"общая сумма: {total}")
+print(f"средняя сумма:{average}")
